d5f31f605b58063d545fedc0a5fa8186f245ee38
NVC_HP <mr.chernukh@gmail.com>
2022-09-15 22:17:21 +0300

    add method ___findSymbolOccurance___

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,24 @@ package com.company;
 public class Main {
 
   public static void main(String[] args) {
-    findSymbolOccurance("Молоко", 'о');
+    findSymbolOccurance("молоко", 'о');
 
   }
 
   static void findSymbolOccurance(String word, char ch) {
     int counter = 0;
+    if (word == null) {
+      System.out.println("В методе пустая строка!");
+      return;
+    }
     char[] strWorld = word.toCharArray();
-    for (int i = 0; i < strWorld.length; i++) {
-      if (strWorld[i] == ch) {
+    for (char c : strWorld) {
+      if (c == ch) {
         counter++;
       }
     }
-    System.out.println(
-        "Количество символов " + "'" + ch + "'" + " в слове " + word + " = " + counter);
+    System.out.println("Количество символов " + "'" +
+        ch + "'" + " в слове " + word + " = " + counter);
   }
 
   static void findWordPosition(String source, String target) {
